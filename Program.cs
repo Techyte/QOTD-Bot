73a3ee739a09010639ba8dd3005fb888cab5b4b6
Techyte <owinterh@icloud.com>
2022-11-11 14:53:06 +1100

    Clean-up and commenting

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using DSharpPlus;
+﻿using System.Timers;
+using DSharpPlus;
 using DSharpPlus.Entities;
 using DSharpPlus.EventArgs;
 
@@ -20,6 +21,9 @@ namespace QOTD_Bot
             configData.GuildId = ulong.Parse(Environment.GetEnvironmentVariable("guildId"));
             configData.hour = int.Parse(Environment.GetEnvironmentVariable("hour"));
             
+            Console.WriteLine($"Bot Token: {configData.Token}");
+            Console.WriteLine($"Id of the Server: {configData.GuildId}");
+            Console.WriteLine($"Id of the Channel: {configData.ChannelId}");
             Console.WriteLine($"Question will be asked at {configData.hour} o'clock");
             
             MainAsync().GetAwaiter().GetResult();
@@ -37,54 +41,30 @@ namespace QOTD_Bot
             {
                 if (e.Channel.Type == ChannelType.Private)
                 {
-                    if (!e.Message.Author.IsBot)
-                    {
-                        e.Message.CreateReactionAsync(DiscordEmoji.FromUnicode("👍"));
-                        possibleQuestions.Add(e.Message.Id, e.Message);
-                        Console.WriteLine($"Received a question from {e.Author.Username}: {e.Message.Content}.");
-                    }
+                    QuestionAsked(e);
                 }
             };
 
             discord.MessageDeleted += async (s, e) =>
             {
-                possibleQuestions.Remove(e.Message.Id);
-                Console.WriteLine($"Question from {e.Message.Author} removed: {e.Message.Content}");
+                if (e.Channel.Type == ChannelType.Private)
+                {
+                    QuestionDeleted(e);
+                }
             };
 
+            // Basically when the Bot has started up
             discord.GuildDownloadCompleted += GetQuestions;
             
+            // The timer that checks every minute to see if it is time to ask the question
             var aTimer = new System.Timers.Timer(60000);
+            
             aTimer.Elapsed += (o, i) =>
             {
-                DateTime time = DateTime.Now;
-                
-                if (time.Hour == configData.hour && time.Minute == 00 && time.ToString("t") == "A")
-                {
-                    if(discord.Guilds.TryGetValue(configData.GuildId, out DiscordGuild guild))
-                    {
-                        if(guild.Channels.TryGetValue(configData.ChannelId, out DiscordChannel channel))
-                        {
-                            Random random = new Random();
-
-                            int randomId = random.Next(0, possibleQuestions.Count);
-                            List<ulong> IdList = new List<ulong>(possibleQuestions.Keys);
-                            
-                            DiscordMessage message;
-                            possibleQuestions.TryGetValue(IdList[randomId], out message);
-                            
-                            DiscordMessage questionMessage = channel.SendMessageAsync($"Question of the day is: {message.Content}").GetAwaiter().GetResult();
-
-                            message.CreateReactionAsync(DiscordEmoji.FromUnicode("✔"));
-                            message.RespondAsync($"This question was asked! Check it out here: {questionMessage.JumpLink}");
-                            Console.WriteLine($"A question was asked! Question content: {questionMessage.JumpLink}.");
-                        }
-                    }
-                }
+                CheckIfTime(discord);
             };
             
             aTimer.AutoReset = true;
-            
             aTimer.Enabled = true;
             
             await discord.ConnectAsync();
@@ -92,19 +72,81 @@ namespace QOTD_Bot
             await Task.Delay(-1);
         }
 
-        private async static Task GetQuestions(DiscordClient discord, GuildDownloadCompletedEventArgs e)
+        // Run when a question is deleted in a DM
+        private static void QuestionDeleted(MessageDeleteEventArgs e)
+        {
+            if (!e.Message.Author.IsBot)
+            {
+                possibleQuestions.Remove(e.Message.Id);
+                Console.WriteLine($"Question from {e.Message.Author} removed: {e.Message.Content}");   
+            }
+        }
+
+        // Run when a question is asked in a DM
+        private static void QuestionAsked(MessageCreateEventArgs e)
+        {
+            if (!e.Message.Author.IsBot)
+            {
+                e.Message.CreateReactionAsync(DiscordEmoji.FromUnicode("👍"));
+                possibleQuestions.Add(e.Message.Id, e.Message);
+                Console.WriteLine($"Received a question from {e.Author.Username}: {e.Message.Content}.");
+            }
+        }
+
+        // Checks if it is time to ask the question
+        private static void CheckIfTime(DiscordClient discord)
+        {
+            DateTime time = DateTime.Now;
+                
+            if (time.Hour == configData.hour && time.Minute == 00 && time.ToString("t") == "A")
+            {
+                AskQuestion(discord);
+            }
+        }
+
+        // Asks the question
+        private static void AskQuestion(DiscordClient discord)
         {
             if(discord.Guilds.TryGetValue(configData.GuildId, out DiscordGuild guild))
             {
+                if(guild.Channels.TryGetValue(configData.ChannelId, out DiscordChannel channel))
+                {
+                    Random random = new Random();
+
+                    int randomId = random.Next(0, possibleQuestions.Count);
+                    List<ulong> IdList = new List<ulong>(possibleQuestions.Keys);
+                            
+                    DiscordMessage message;
+                    possibleQuestions.TryGetValue(IdList[randomId], out message);
+                            
+                    DiscordMessage questionMessage = channel.SendMessageAsync($"Question of the day is: {message.Content}").GetAwaiter().GetResult();
+
+                    message.CreateReactionAsync(DiscordEmoji.FromUnicode("✔"));
+                    message.RespondAsync($"This question was asked! Check it out here: {questionMessage.JumpLink}");
+                    Console.WriteLine($"A question was asked! Question content: {questionMessage.Content}.");
+                }
+            }
+        }
+
+        // Gets the questions that were already given to the bot before it was turned on
+        private async static Task GetQuestions(DiscordClient discord, GuildDownloadCompletedEventArgs e)
+        {
+            Console.WriteLine("Started checking for messages this may take some time depending on the number of members in your server");
+            
+            if(discord.Guilds.TryGetValue(configData.GuildId, out DiscordGuild guild))
+            {
+                // Goes through every member in the server
                 foreach (DiscordMember member in guild.GetAllMembersAsync().GetAwaiter().GetResult())
                 {
                     if (!member.IsBot)
                     {
+                        // Opens the members DM with the bot
                         DiscordChannel channel = member.CreateDmChannelAsync().GetAwaiter().GetResult();
                         foreach (DiscordMessage message in channel.GetMessagesAsync().GetAwaiter().GetResult())
                         {
                             if (!message.Author.IsBot)
                             {
+                                // To make sure that the message has not been ticked (meaning that it has been asked)
                                 int reactions = 0;
                                 foreach (DiscordReaction reaction in message.Reactions)
                                 {
@@ -113,7 +155,7 @@ namespace QOTD_Bot
                                         reactions++;
                                     }
                                 }
-
+                                
                                 if (reactions == message.Reactions.Count)
                                 {
                                     possibleQuestions.Add(message.Id, message);
@@ -124,6 +166,8 @@ namespace QOTD_Bot
                     }
                 }
             }
+            
+            Console.WriteLine("Finished checking for messages");
         }
     }
 
